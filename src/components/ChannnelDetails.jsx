da495bc72e0a7fb985655ec0f8b8b2fb27be44e2
Mohammad Pesaraklou <mhmdpsr6@gmail.com>
2022-11-25 05:53:27 +0330

    start the project

---
+++ src/components/ChannnelDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChannnelDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ChannnelDetails;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// logo
+import { logo } from "../utils/constants";
+const Navbar = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Navbar;
